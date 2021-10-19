f60f6c50a7163172e870517d4ca17bb9ce0d6617
Yaswanth <yaswanthp.in@mouritech.com>
2021-10-19 12:54:25 +0530

    added test case for get method

--- src/jdbc/JDBCTest.java
+++ src/jdbc/JDBCTest.java
@@ -1,14 +1,48 @@
 package jdbc;
 
-import java.sql.SQLException;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class JDBCTest {
 	
-	@Test
-	public void createTableTest() throws SQLException {
+	JDBC jdbc = new JDBC();
+	static Statement stm;
+	
+	@BeforeAll
+	public static void setup() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			//connect to database
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc", "root", "Yaswanth@1994");
+			//creating new statement
+			stm = conn.createStatement();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+	}
+	
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void getAllPersonsTest() throws SQLException {
+		String response = jdbc.getAllPersons(stm);
+		assertEquals("fetch data successfull", response);
 	}
 
 }
